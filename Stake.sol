bb1367c5cb230102efab358677488e5151bcc376
Agacja <agacja@icloud.com>
2023-08-26 19:58:04 +0200

    adding lib

--- Stake.sol
+++ Stake.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "erc721a/contracts/IERC721A.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/ERC721A/contracts/IERC721A.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "lib/openzeppelin-contracts//contracts/token/ERC721/utils/ERC721Holder.sol";
 
 contract KochamCie is Ownable, ERC721Holder {
    
