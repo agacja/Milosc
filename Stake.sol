81863c0f6e918a291dde0f96f881c28bd6d9a9ed
Agacja <agacja@icloud.com>
2023-12-29 18:04:44 +0100

    update

--- Stake.sol
+++ Stake.sol
@@ -1,229 +1,180 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import "lib/ERC721A/contracts/IERC721A.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import "lib/openzeppelin-contracts//contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/IERC721A.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "solady/src/utils/SafeTransferLib.sol";
 
-contract KochamCie is Ownable, ERC721Holder {
+
+contract Milosc is Ownable, ERC721Holder {
    
-  mapping(address => mapping(uint256 => bool)) private data;
+  mapping(address => mapping(uint256 => bool)) private pupa;
   mapping(address => mapping(uint256 => uint256)) private _userStakingTimestamp;
-  mapping (address => mapping(uint256 =>bool)) private collected;
- 
-
-    using SafeERC20 for IERC20;
-    address private _MILOSCSTAKING;
+  mapping(address => mapping(uint256 => uint256)) private miloscStakingInfosTime;
+  mapping(address => mapping(uint256 =>address)) private userdata;
+  mapping(address => address) private collectiontokentype;
+   
+  using SafeERC20 for IERC20;
+    
+    address private _LoveStaking;
     uint256 private DaoValue;
+    uint256 private lifeSpan;
+    address public safeadd;
+
 
  
-   function stake(uint256 tokenIds) external {
-        bytes4 transferFrom = 0x23b872dd;
-        assembly{
-        let ptr := mload(0x40)
-            //Get location of this address and token in _userStakingData
-            mstore(0x0, caller())
-            mstore(0x20, data.slot)
+function stake(uint256 tokenIds, address _nft) external {
+
+   assembly{
+            mstore(0x0, _nft)
+            mstore(0x20, userdata.slot)
+            mstore(0x40, tokenIds) // Use 0x40 - no overriding
+
+           
+            let locations := keccak256(0x0, 0x60) 
+            if sload(locations) {
+                mstore(0x0, 0x01336cea) // 'overflow' selector
+                  revert(0x1c, 0x04) 
+            }
+
+            sstore(locations, _nft)
+           
+         let currentTime := timestamp()
+            mstore(0x0, _nft)
+            mstore(0x20, miloscStakingInfosTime.slot)
             mstore(0x20, keccak256(0x0, 0x40))
             mstore(0x00, tokenIds)
+            
 
          let location := keccak256(0x0, 0x40)
 
-   if sload(location) {
-               mstore(0x0, 0x039f2e18) //'NotStaked()' selector
+          if sload(location) {
+               mstore(0x0,  0x01336cea) //"overflow' selector
                 revert(0x1c, 0x04)
             }
-             sstore(keccak256(0x0, 0x40), 0x1)
+            sstore(keccak256(0x0, 0x40), currentTime)
            
-        
-         let currentTime := timestamp()
- // Update _userStakingTimestamp
-        mstore(0x20, _userStakingTimestamp.slot)
-        mstore(0x20, keccak256(0x0, 0x40))
-        mstore(0x00, tokenIds)
-        sstore(keccak256(0x0, 0x40), currentTime)
+              
+ 
+ let stakingContract := sload(_LoveStaking.slot)
 
-         
-            // store transferFrom selector
-            let transferFromData := add(0x20, mload(0x40))
-            let stakingContract := sload(_MILOSCSTAKING.slot)
-            mstore(transferFromData, transferFrom)
-            // store caller address
-            mstore(add(transferFromData, 0x04), caller())
-            // store address
-            mstore(add(transferFromData, 0x24), stakingContract)
-            // store _total
-            mstore(add(transferFromData, 0x44), tokenIds)
+        mstore(0x00, hex"23b872dd") 
+       
+        mstore(0x04, caller())
+   
+        mstore(0x24, stakingContract)
+ 
+        mstore(0x44, tokenIds)
 
-           let successTransferFrom := call(
-                gas(),
-                0x571ae97bde80Fc2d53D66bfDeAe5Ba9Af34cEB43,
-                0,
-                transferFromData,
-                0x64, // Size of the transferFromData (100 bytes)
-                0,
-                0
-            )
-
-            if iszero(successTransferFrom) {
-                revert(0, 0)
-            }
-        } 
+        if iszero(call(gas(), _nft, 0, 0x00, 0x64, 0, 0)) {
+            revert(0, 0)
+        }
     }
-  
+}
 
        
-  function unstake(uint256 tokenIds) external {
-   
-            bytes4 transferFrom = 0x23b872dd; 
+  function unstake(uint256 tokenIds,address _nft) external {
             
            address reciever = msg.sender;
-       
-           assembly {
-            //Cache Free memory pointer
-            let ptr := mload(0x40)
-          //Cache _userStakingData location for this address.
-         mstore(0x0, caller())
-            mstore(0x20, data.slot)
-            mstore(0x20, keccak256(0x0, 0x40))
-            mstore(0x0, tokenIds)
-            let location := keccak256(0x0, 0x40)
+       assembly{
+            mstore(0x0, _nft)
+            mstore(0x20, userdata.slot)
+            mstore(0x40, tokenIds)
 
-          //If not staked revert NotStaked()
-            if iszero(sload(location)) {
-               mstore(0x0, 0x039f2e18) //'NotStaked()' selector
+            
+            let location := keccak256(0x0, 0x60)
+            let stakedNft := sload(location)
+            if iszero(stakedNft) {
+           
+                mstore(0x0, 0x039f2e18) // 'NotStaked()' selector
                 revert(0x1c, 0x04)
             }
-            //Update _userStakingData mapping
-            
-       // Get the timestamp when the token was staked
-        mstore(0x20, _userStakingTimestamp.slot)
+             sstore(location, 0x0)
+    
+        mstore(0x0, _nft)   
+        mstore(0x20, miloscStakingInfosTime.slot)
         mstore(0x20, keccak256(0x0, 0x40))
         mstore(0x00, tokenIds)
         let stakeTimestamp := sload(keccak256(0x0, 0x40))
-
-        // Check if 2 minutes have passed since staking
+    
+    
         let currentTime := timestamp()
         let timeElapsed := sub(currentTime, stakeTimestamp)
+         //let lifeSpans := mload(lifeSpan.slot)
         if lt(timeElapsed, 120) {
             mstore(0x00, 0x039f2e18) // 'NotStaked()' selector
             revert(0x1c, 0x04)
         }
+ 
+        sstore(keccak256(0x0, 0x40), 0x0) 
+
        
-            let transferFromData := add(0x20, mload(0x40))
+            let stakingContract := sload(_LoveStaking.slot)
             
-            mstore(transferFromData, transferFrom)
-            //let sender := reciever
-            let stakingContract := sload(_MILOSCSTAKING.slot)
-            
-  
-             mstore(add(transferFromData, 0x04), stakingContract)
-             mstore(add(transferFromData, 0x24), reciever)
-             mstore( add(transferFromData, 0x44),tokenIds)
+             mstore(0x00, hex"23b872dd")
+             mstore(0x04, stakingContract)
+             mstore(0x24, reciever)
+            mstore(0x44, tokenIds)
 
-            
-
-           let successTransferFrom := call(
-                    gas(),
-                    0x571ae97bde80Fc2d53D66bfDeAe5Ba9Af34cEB43,
-                    0,
-                    transferFromData,
-                    0x64,
-                    0,
-                    0
-                )
-
-           // revert if call fails
-                if iszero(successTransferFrom) {
+       if iszero(call(gas(),_nft, 0, 0x00, 0x64, 0, 0)) {
                     revert(0, 0)
-                } 
-         sstore(location, 0x0)
+                }
+       
         } }
 
-  // Define the contract and its state variables here (including DaoValue and other required mappings/variables)
 
-function collectRewards(uint256 tokenIds) external {
-    bytes4 transferFrom = 0x23b872dd;
+function collectRewards(uint256 tokenIds, address _nft) external {
     address receiver = msg.sender;
-    address er = 0xeFeECadFF1E463481aB53Ba91Ad6ac376CdC68D4;
-    
+    address tresury = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    address token = collectiontokentype[_nft];
+
     assembly {
-        // Get the timestamp when the token was staked
-        mstore(0x20, _userStakingTimestamp.slot)
+        // Load the location of the staked timestamp
+        mstore(0x20, miloscStakingInfosTime.slot)
         mstore(0x00, tokenIds)
-        let stakeTimestamp := sload(keccak256(0x0, 0x40))
-        
-        // Check if 120 seconds  have passed since the start of the stake
+        let stakeTimestampLocation := keccak256(0x20, 0x20)
+        let stakeTimestamp := sload(stakeTimestampLocation)
+
+        // Check if at least 120 seconds have passed since the stake
         let currentTime := timestamp()
         let timeElapsed := sub(currentTime, stakeTimestamp)
         if lt(timeElapsed, 120) {
             mstore(0x00, 0x039f2e18) // 'NotStaked()' selector
             revert(0x1c, 0x04)
         }
-
-        // Cache 'collected' location for this address.
-        mstore(0x0, caller())
-        mstore(0x20, collected.slot)
-        mstore(0x0, tokenIds)
-        let location := keccak256(0x0, 0x40)
-
-        if sload(location) {
-            mstore(0x00, 0x639c3fa4) // 'collected()' selector
-            revert(0x1c, 0x04)
-        }
-      // Get the timestamp when the token was staked
- 
-        //sstore(DaoValue.slot, 0x1)
-
-        // Calculate reward based on 'DaoValue' and days staked
+       // Calculate reward based on 'DaoValue' and days staked
         let daoValue := sload(DaoValue.slot)
         let currentTimes := timestamp()
         let daysStaked := div(sub(currentTimes, stakeTimestamp), 86400)
         let reward := mul(daoValue,daysStaked)
    
 
-        // Prepare 'transferFrom' data for ERC-20 token transfer
-        let transferFromData := add(0x20, mload(0x40))
-        mstore(transferFromData, transferFrom)
-        mstore(add(transferFromData, 0x04), er)//0x2870A2e52Caa18617d1cd5d5374A07b149bc74B5
-        mstore(add(transferFromData, 0x24), receiver)
-        mstore(add(transferFromData, 0x44), reward)
+            mstore(0x00, hex"23b872dd")
+            mstore(0x04, tresury)
+            mstore(0x24, receiver)
+            mstore(0x44, reward)
 
-        // Perform the ERC-20 token transfer using the 'transferFrom' function
-        let successTransfer := call(
-            gas(),
-            0x322D3c79F0F92861124D702c90ce0d4737094AC8,
-            0,
-            transferFromData,
-            0x64,
-            0,
-            0
-        )
-
-        // Check if the transfer was successful
-        if iszero(successTransfer) {
-            revert(0, 0)
-        }
-
+            if iszero(call(gas(),token, 0, 0x00, 0x64, 0, 0)) {
+                revert(0, 0)
+            }
         // Mark the rewards as collected for this address and token ID
-       sstore(location, 0x1)
-    }
-}
+       // sstore(collectedLocation, 0x1)
+    }}
 
-    
     function zgarnijNft (uint256 tokenIds) external onlyOwner {
             // Get the timestamp when the token was staked
             bytes4 transferFrom = 0x23b872dd;
             address skarbiec = 0x13d8cc1209A8a189756168AbEd747F2b050D075f;
-               
-
+         
+        
         assembly {
          //Cache Free memory pointer
             let ptr := mload(0x40)
           //Cache _userStakingData location for this address.
          mstore(0x0, caller())
-            mstore(0x20, data.slot)
+            mstore(0x20, pupa.slot)
             mstore(0x20, keccak256(0x0, 0x40))
             mstore(0x0, tokenIds)
             let location := keccak256(0x0, 0x40)
@@ -239,7 +190,7 @@ function collectRewards(uint256 tokenIds) external {
         mstore(0x00, tokenIds)
         let stakeTimestamp := sload(keccak256(0x0, 0x40))
 
-        // Check if 365 days minutes have passed since staking
+        // Check if 500 seconds have passed since staking
         let currentTime := timestamp()
         let timeElapsed := sub(currentTime, stakeTimestamp)
         if lt(timeElapsed, 500) {
@@ -251,35 +202,29 @@ function collectRewards(uint256 tokenIds) external {
             
             mstore(transferFromData, transferFrom)
             //let skarbiec := studnia
-            let stakingContract := sload(_MILOSCSTAKING.slot)
+            let stakingContract := sload(_LoveStaking.slot)
             
   
-             mstore(add(transferFromData, 0x04), stakingContract)
-             mstore(add(transferFromData, 0x24), skarbiec)
-             mstore( add(transferFromData, 0x44),tokenIds)
+            mstore(0x00, hex"23b872dd")
+            mstore(0x04, stakingContract)
+            mstore(0x24, skarbiec)
+            mstore(0x44, tokenIds)
+          
+  if iszero(call(gas(),0x20a78762085602705007DCB326e33EA71C8d1f6F, 0, 0x00, 0x64, 0, 0)) {
+                revert(0, 0)
+            }
 
-            
-
-           let successTransferFrom := call(
-                    gas(),
-                    0x571ae97bde80Fc2d53D66bfDeAe5Ba9Af34cEB43,
-                    0,
-                    transferFromData,
-                    0x64,
-                    0,
-                    0
-                )
-
-            
-           if iszero(successTransferFrom) {
-                    revert(0, 0)
-           }
           
         }}
      
 
-   function setMILOSCSTAKING(address MILOSCSTAKING_) external onlyOwner {
-        _MILOSCSTAKING = MILOSCSTAKING_;
+function updateCollectionToken(address _nft, address _token) external onlyOwner {
+    collectiontokentype[_nft] = _token;
+}
+
+
+   function setLoveStaking(address LoveStaking_) external onlyOwner {
+        _LoveStaking = LoveStaking_;
    }
     function setDaoValue(uint256 _DaoValue) external onlyOwner {
         DaoValue =  _DaoValue;
@@ -287,12 +232,21 @@ function collectRewards(uint256 tokenIds) external {
    function getDaoValue() external view returns (uint256) {
         return DaoValue;
     }
- function userStakingData(address staker, uint256 tokenId)
+
+       function setlifespan(uint256 _lifeSpan) external onlyOwner {
+        lifeSpan =  _lifeSpan;
+   }
+ function userStakingmiloscStakingInfosTime(address staker, uint256 tokenId)
         external
         view
-        returns (bool)
+        returns (uint256)
     {
-        return data[staker][tokenId];
+        return miloscStakingInfosTime[staker][tokenId];
+    }
+     // Function to withdraw both Ethereum and ERC20 tokens from the contract.
+    function withdrawTokens(address _token) external payable onlyOwner {
+        uint256 _Balance = IERC20(_token).balanceOf(address(this));
+        SafeTransferLib.safeTransfer(_token, safeadd, _Balance);
     }
 
     }
